0ee26db6c677583b98c19433bc7908831ebc76af
Youjin Park <youjin09222@naver.com>
2022-01-22 20:09:33 +0900

    버튼

--- app/src/main/java/com/example/vegandiary/Login.kt
+++ app/src/main/java/com/example/vegandiary/Login.kt
@@ -24,7 +24,7 @@ class Login : AppCompatActivity() {
         edit_ID = findViewById<EditText>(R.id.edit_ID)
 
         Login_Btn.setOnClickListener{
-            val intent = Intent(this, SecondActivity::class.java) //화면 전환
+            val intent = Intent(this, MainActivity::class.java) //화면 전환
             // id가 다음 화면에 보임
             intent.putExtra("User", edit_ID.text.toString()) //key:WMI, value : ed_ID.text
             Toast.makeText(this, "회원가입 완료!", Toast.LENGTH_SHORT).show()
