c939e88770c6d3d831b7f66f9f287092318be6c8
Youjin Park <youjin09222@naver.com>
2022-01-27 23:50:51 +0900

    로그인 구현 (id : 1111@gmail.com Pw : 1111)

---
+++ app/src/main/java/com/example/vegandiary/model/User.kt
@@ -0,0 +1,9 @@
+package com.example.vegandiary.model
+
+// 데이터 저장
+class User {
+    var id = 0
+    var name: String? = null //null 허용
+    var email: String? = null
+    var password: String? = null
+}
