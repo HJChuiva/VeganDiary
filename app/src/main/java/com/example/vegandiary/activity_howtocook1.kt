82d0ece5488920ba00feae61fba93a4c41554c91
Hihee <gjgmlwnrnrm@gmail.com>
2022-01-28 13:05:36 +0900

    하우 투 쿡!

---
+++ app/src/main/java/com/example/vegandiary/activity_howtocook1.kt
@@ -0,0 +1,11 @@
+package com.example.vegandiary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_howtocook1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_howtocook1)
+    }
+}
