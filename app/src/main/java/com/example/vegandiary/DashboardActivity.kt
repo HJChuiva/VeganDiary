66beb2035fb7f401aa78b17f530402233988efa4
HJChuiva <95302018+HJChuiva@users.noreply.github.com>
2022-01-30 18:32:33 +0900

    SearchTextButton

--- app/src/main/java/com/example/vegandiary/DashboardActivity.kt
+++ app/src/main/java/com/example/vegandiary/DashboardActivity.kt
@@ -1,30 +1,39 @@
 package com.example.vegandiary
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.vegandiary.Fragment.*
+import com.example.vegandiary.Fragment.SearchFragment
+import com.example.vegandiary.DashboardActivity as DashboardActivity1
+
 
 
 class DashboardActivity : AppCompatActivity()  {
+    var context: Context? = null
 
-    lateinit var SearchTextButton: Button
-    lateinit var ScrapButton: Button
+    lateinit var searchTextButton: Button
+
+
+    private val searchFragment = SearchFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_dashboard)
 
-        SearchTextButton=findViewById<Button>(R.id.SearchTextButton)
-        ScrapButton=findViewById<Button>(R.id.ScrapButton)
+        searchTextButton=findViewById<Button>(R.id.SearchTextButton)
 
-        SearchTextButton.setOnClickListener{
-            val intent = Intent(this, SearchActivity::class.java)
+        searchTextButton.setOnClickListener {
+            val intent = Intent(this, SearchActivity::class.java);
             startActivity(intent)
-
+            }
         }
-        ScrapButton.setOnClickListener{
 
-        }
-    }
+
+
 }
+
+
