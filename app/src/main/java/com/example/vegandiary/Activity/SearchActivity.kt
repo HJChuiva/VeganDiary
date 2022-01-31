2d9da7fff0bbfa44068d2f53c9984a512f5cfc3b
HJChuiva <95302018+HJChuiva@users.noreply.github.com>
2022-02-01 07:28:33 +0900

    시작 스플래시 + 튜토리얼 구성

--- app/src/main/java/com/example/vegandiary/Activity/SearchActivity.kt
+++ app/src/main/java/com/example/vegandiary/Activity/SearchActivity.kt
@@ -1,12 +1,24 @@
-package com.example.vegandiary.Activity
+package com.example.veganDiary.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.vegandiary.Activity.CustomAdapter
 import com.example.vegandiary.R
+import com.example.vegandiary.Activity.com.example.vegandiary.sql.RecipeData
+import kotlinx.android.synthetic.main.scroll_search_recipe.*
+
 
 class SearchActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_search)
+        setContentView(R.layout.activity_search)
+
+        val list = ArrayList<RecipeData>()
+        list.add(RecipeData(null,"Signature brunch menu"))
+        list.add(RecipeData(null,"A picnic menu"))
+
+        val adapter=CustomAdapter(list)
+        scroll_search_recipe.adapter=adapter
     }
 }
---
+++ app/src/main/java/com/example/vegandiary/Intro/IntroRecyclerAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.vegandiary.Activity.com.example.vegandiary.Intro
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.vegandiary.R
+
+class IntroRecyclerAdapter (private  var pageList: ArrayList<PageItem>)
+    : RecyclerView.Adapter<MyPagerViewholder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyPagerViewholder {
+        return  MyPagerViewholder(LayoutInflater.from(parent.context).inflate(
+            R.layout.item_intro_pager,parent,false))
+    }
+
+    override fun getItemCount(): Int {
+        return pageList.size
+    }
+    //데이터와 뷰 묶기
+    override fun onBindViewHolder(holder: MyPagerViewholder, position: Int) {
+        holder.bindWithView(pageList[position])
+    }
+
+
+}
+
+
+
+
+
