cdada2fb94df5c32951744e17094e40b3ae83c20
HJChuiva <95302018+HJChuiva@users.noreply.github.com>
2022-02-07 00:55:55 +0900

    커미잇

--- app/src/main/java/com/example/vegandiary/Intro/PageItem.kt
+++ app/src/main/java/com/example/vegandiary/Intro/PageItem.kt
@@ -1,4 +1,4 @@
 package com.example.vegandiary.Activity.com.example.vegandiary.Intro
 
-class PageItem(val bgColor: Int, val image: Int, val text1:String, val text2: String, val text3: String){
+class PageItem(val bgColor: Int, val image: Int, val text1:String, val text2: String){
     }
