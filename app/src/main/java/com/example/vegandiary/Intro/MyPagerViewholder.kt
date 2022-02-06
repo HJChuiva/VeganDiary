6550b8ad5b844aed2950e6c91298f6d8c87eda7b
HJChuiva <95302018+HJChuiva@users.noreply.github.com>
2022-02-07 00:55:32 +0900

    커미잇

--- app/src/main/java/com/example/vegandiary/Intro/MyPagerViewholder.kt
+++ app/src/main/java/com/example/vegandiary/Intro/MyPagerViewholder.kt
@@ -12,13 +12,11 @@ class MyPagerViewholder(itemView: View): RecyclerView.ViewHolder(itemView) {
     private val itemImage=itemView.pager_item_image
     private val itemText1 = itemView.pager_item_text1
     private val itemText2 = itemView.pager_item_text2
-    private val itemText3 = itemView.pager_item_text3
 
     fun bindWithView(pageItem: PageItem){
         itemImage.setImageResource(pageItem.image)
         itemText1.text=pageItem.text1
         itemText2.text=pageItem.text2
-        itemText3.text=pageItem.text3
 
 
     }
